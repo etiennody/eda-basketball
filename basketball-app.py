3e3b06d3fc7c161ed931d3443eb873d33076488e
etiennody <etiennody@gmail.com>
2021-03-22 22:52:08 +0100

    added web scrapping of nba players stats

--- basketball-app.py
+++ basketball-app.py
@@ -15,4 +15,26 @@ st.markdown(
 )
 
 st.sidebar.header("User Input Features")
-select_year = st.sidebar.selectbox("Year", list(reversed(range(1950, 2022))))
+selected_year = st.sidebar.selectbox("Year", list(reversed(range(1950, 2022))))
+
+# Web scrapping of NBA players stats
+@st.cache
+def load_data(year):
+    url = (
+        "https://www.basketball-reference.com/leagues/NBA_"
+        + str(year)
+        + "_per_game.html"
+    )
+    html = pd.read_html(url, header=0)
+    df = html[0]
+    raw = df.drop(df[df.Age == "Age"].index)
+    raw = raw.fillna(0)
+    players_stats = raw.drop(["Rk"], axis=1)
+    return players_stats
+
+
+players_stats = load_data(selected_year)
+
+# Sidebar - Team selection
+sorted_unique_team = sorted(players_stats.Tm.unique())
+selected_team = st.sidebar.multiselect("Team", sorted_unique_team, sorted_unique_team)
